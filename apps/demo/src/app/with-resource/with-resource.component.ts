5b8cb3486fb4804aa1122b15d053fb7372f00ed1
Michael Small <michaelsmalldev@gmail.com>
2026-08-27 19:38:26 -0500

    show example of named resource not narrowing w/hasValue

--- apps/demo/src/app/with-resource/with-resource.component.ts
+++ apps/demo/src/app/with-resource/with-resource.component.ts
@@ -2,45 +2,62 @@ import { withResource } from '@angular-architects/ngrx-toolkit';
 import { JsonPipe } from '@angular/common';
 import { httpResource } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
-import { signalStore, withState } from '@ngrx/signals';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { signalStore, withProps, withState } from '@ngrx/signals';
+import { of } from 'rxjs';
 import { Flight } from '../shared/flight';
 
 const url = 'https://demo.angulararchitects.io/api/flight?from=Paris&to=';
 
 export const FlightStore = signalStore(
   withState({ flightTo: 'New York' }),
-  withResource(({ flightTo }) => httpResource(() => `${url}${flightTo()}`)),
-  withResource(({ flightTo }) => ({
-    list: httpResource<Flight[]>(() => `${url}${flightTo()}`, {
-      defaultValue: [],
+  withResource(
+    ({ flightTo }) => httpResource<Flight[]>(() => `${url}${flightTo()}`),
+    { errorHandling: 'native' },
+  ),
+  withResource(
+    ({ flightTo }) => ({
+      list: httpResource<Flight[]>(() => `${url}${flightTo()}`),
     }),
-  })),
+    { errorHandling: 'native' },
+  ),
+  withProps(() => {
+    return {
+      normalResource: rxResource({
+        stream: () => {
+          return of<Flight[]>([]);
+        },
+      }),
+    };
+  }),
 );
 
 @Component({
   selector: 'demo-with-resource',
   imports: [JsonPipe],
-  template: `,
-    <h1>withResource</h1>
-    <a
-      href="https://ngrx-toolkit.angulararchitects.io/docs/with-resource"
-      target="_blank"
-      >withResource doc page</a
-    >
-
-    <h2>Single Resource</h2>
-    <pre>value: {{ store.value() | json }}</pre>
-    <pre>status: {{ store.status() }}</pre>
-    <pre>error: {{ store.error() | json }}</pre>
-    <pre>hasValue: {{ store.hasValue() }}</pre>
-
-    <h2>Named Resource</h2>
-    <pre>{{ store.listValue() | json }}</pre>
-    <pre>status: {{ store.listStatus() }}</pre>
-    <pre>error: {{ store.listError() | json }}</pre>
-    <pre>hasValue: {{ store.listHasValue() }}</pre> `,
+  templateUrl: './with-resource.component.html',
   providers: [FlightStore],
 })
 export class WithResourceComponent {
   store = inject(FlightStore);
+
+  constructor() {
+    // A regular resource
+    this.store.normalResource.value();
+    if (this.store.normalResource.hasValue()) {
+      this.store.normalResource.value();
+    }
+
+    // `withResource`
+    // unnamed
+    this.store.value();
+    if (this.store.hasValue()) {
+      this.store.value();
+    }
+    // named
+    this.store.listValue();
+    if (this.store.listHasValue()) {
+      this.store.listValue(); // not narrowing
+    }
+  }
 }
